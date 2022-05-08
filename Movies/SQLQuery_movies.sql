2a598e0b9ed6f1760d884921a16a10b591fcd71e
Ann.Piotrowska <anna.piotrowska.pl@gmail.com>
2022-05-08 23:23:26 +0200

    Upload Movies project

--- Movies/SQLQuery_movies.sql
+++ Movies/SQLQuery_movies.sql
@@ -1,103 +1,90 @@
-SELECT *
-FROM MyProjects..Movies
 
--- 10 najlepszych filmów, które trwaja poni¿ej 100 minut
+-- display top 10 videos that are under 100 minutes long
 
 SELECT TOP(10) Title, Director, Year, Runtime, Rating
-FROM MyProjects..Movies
+FROM Movies
 WHERE Runtime <= 100
 ORDER BY Rating DESC
 
--- który rezyser ma najwyzsze przychody - top 3
+-- display three directors and with the highest revenues
 
 SELECT TOP(3) Director, MAX(CAST(Revenue AS float)) AS max_revenue
-FROM MyProjects..Movies
+FROM Movies
 GROUP BY Director
 ORDER BY max_revenue DESC
 
--- najgorsze filmy z przychodami powy¿ej 100 tyœ
+-- display the worst films with revenues over 100.000
 
 SELECT [Rank], Title, CAST(Revenue AS float) Revenue, Rating
-FROM MyProjects..Movies
+FROM Movies
 WHERE CAST(Revenue AS float) >100
 ORDER BY Rating
 
--- najgorszy horror 
+-- display worst horror
 
 SELECT TOP (1) [Rank], Title, Genre, Rating
-FROM MyProjects..Movies
+FROM Movies
 WHERE Genre LIKE '%Horror%'
 ORDER BY Rating
 
 
--- aktor i re¿yser w jednym
+-- display actor and director in one person
 
 SELECT Title, Director, Actors
-FROM MyProjects..Movies
+FROM Movies
 WHERE Actors like '%' + Director + '%'
 
 
--- wyswietlenie listy 10 najbardziej dochodowych filmow (w milionach dolarow)
+--display the list of the top 10 highest-grossing films (in million dollars)
 
 SELECT TOP(10) Title, Director, Actors, Year, Rating, CHAR(36)+Revenue + ' M' AS Revenue
-FROM MyProjects..Movies
+FROM Movies
 ORDER BY CAST(Revenue AS float) DESC
 
 
--- oceny filmow w ktorych zagral Brad Pitt
+--display ratings of films in which he starred Brad Pitt
 
 SELECT Actors, Title, Year, CAST(Rating AS float)
-FROM MyProjects..Movies
+FROM Movies
 WHERE Actors LIKE '%Brad Pitt%'
 ORDER BY Year
 
--- œrednie ocena filmow w ktorych zagral Ryan Reynolds a Brad Pitt
 
-SELECT 'Brad Pitt' As Actor, AVG(CAST(Rating AS float))
-FROM MyProjects..Movies
-WHERE Actors LIKE '%Brad Pitt%'
-
-SELECT 'Ryan Reynolds' As Actor, AVG(CAST(Rating AS float))
-FROM MyProjects..Movies
-WHERE Actors LIKE '%Ryan Reynolds%'
-
---œrednia ocena filmow dla kazdego rezysera
+--dispaly average rating of films for each director
 
 SELECT Director, COUNT(Title), ROUND(AVG(CAST(Rating AS float)),2) AS avg_rating
-FROM MyProjects..Movies
+FROM Movies
 GROUP BY Director
 ORDER BY avg_rating DESC
 
-SELECT *
-FROM MyProjects..Movies
-WHERE Director = 'Christopher Nolan'
 
---œrednia ocena filmow dla rezysera, który ma na koncie co najmniej 4 filmy
+--dispaly average film rating for each director who has directed at least four films
 
 SELECT Director, COUNT(Title), ROUND(AVG(CAST(Rating AS float)),2) AS avg_rating
-FROM MyProjects..Movies
+FROM Movies
 GROUP BY Director
 HAVING COUNT(Title) >= 4
 ORDER BY avg_rating DESC
 
--- wyswietlenie listy wszystkich rezyserów wraz z ich inicja³ami
+
+--display a list of all directors with their initials
 
 SELECT DISTINCT Director, SUBSTRING(Director,1,1) + SUBSTRING(Director,PATINDEX('% %',Director)+1,1) AS initial
-FROM MyProjects..Movies
+FROM Movies
 ORDER BY 1
 
 
---wyszukanie filmów które w tytule zawieraj¹ apostrof
+--search for movies with an apostrophe in the title
 
 SELECT *
-FROM MyProjects..Movies
+FROM Movies
 WHERE Title LIKE '%''%'
 
 
--- opisy do 255 znaków -> wyswietl dane dotyczace filmów, ktore nie mieszcza sie w kolumnie
+-- display data about movies that do not fit in the column, because they have descriptions up to 255 characters
 
 SELECT Title, Description, LEN(Description) AS length
-FROM MyProjects..Movies
+FROM Movies
 WHERE LEN(Description) >= 255
 ORDER BY length DESC
 
